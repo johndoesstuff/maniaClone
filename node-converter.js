f09c65f22e616ebe0815e8f5de31e3751721150f
unknown <johndoesstuff03@gmail.com>
2021-10-03 22:16:04 -0400

    Update node-converter.js

--- node-converter.js
+++ node-converter.js
@@ -2,3 +2,4 @@ var fs = require('fs');
 var StreamZip = require('node-stream-zip');
 var zip = new StreamZip({file: './test map.osz', storeEntries: true});
 var entries = zip.entries();
+Array(Object.keys(entries).filter(e => (["osu", "mp3"]).includes(e.split(".")[e.split(".").length-1])).length).fill(0).map((e, i) => entries[Object.keys(entries).filter(e => (["osu", "mp3"]).includes(e.split(".")[e.split(".").length-1]))[i]])
